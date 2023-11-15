f420d3edc52a3da34794b07978219b4c604f1ba2
elishaajayi <ajayienterprises.dev@gmail.com>
2023-11-15 06:46:43 +0100

    Fixed

--- shell_version_two.h
+++ shell_version_two.h
@@ -5,6 +5,7 @@
 #define ONLINE 1
 #define MAX_NUM_ARGUMENTS 64
 #define MAX_INPUT_SIZE 1024
+#define BUFFER 1024
 
 /* Inclusions */
 #include <stdio.h>
@@ -24,9 +25,7 @@ char **tokenizer(char *input);
 int handle_var(char *command, char *arguments[], int cmd_status, int pid);
 int builtin_exit(char *command, char *arguments[], int cmd_status);
 int builtin_env(char *command, char **environ);
-void get_coc(int singh);
-ssize_t my_getlinef(char *lineptr, size_t *n,File *stream);
-void bring_line(char *lineptr, size_t *n, char *buff, size_t g);
+ssize_t my_getlinef(char *lineptr, size_t *n, FILE *stream);
 
 /* External Variables Needed */
 extern char **environ;
