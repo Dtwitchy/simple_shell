5ea663671cb513a25f1959f98af2fef7da79e866
Dtwitchy <destiny.aghafekokhian200@gmail.com>
2023-11-15 02:36:46 +0100

    done

---
+++ my_getlinef.c
@@ -0,0 +1,12 @@
+#include "shell_version_two.h"
+
+/**
+ * my_getlinef: read an entr line from stream
+ * @linepter: A pter to the buf where the line stro
+ * @n: A pter to the size of the buffer
+ * @stream: Stream to read from
+ * Return: number of bytes
+ */
+ssize_t my_getlinef(char *lineptr, size_t *n,File *stream)
+{
+}
